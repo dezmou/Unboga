783502aa29503a258ddc0194b0640255c2319102
Dezmou <incredibledez@gmail.com>
2022-11-02 19:37:41 +0100

    wip

--- lineup/back/src/bdd.ts
+++ lineup/back/src/bdd.ts
@@ -1,6 +1,9 @@
 import { readFile, writeFile } from "fs"
 
+const cache: { [key: string]: string } = {};
+
 export const getItem = (id: string) => {
+    if (cache[id]) return cache[id];
     return new Promise<string>((r, j) => {
         readFile(`./bdd/${id}`, "utf-8", (err, data) => {
             if (err) {
@@ -12,8 +15,9 @@ export const getItem = (id: string) => {
     })
 }
 
-export const setItem = (id: string, data: string) => {
-    return new Promise<void>((r, j) => {
+export const setItem = async (id: string, data: string) => {
+    cache[id] = data;
+    await new Promise<void>((r, j) => {
         writeFile(`./bdd/${id}`, data, "utf-8", (err) => {
             if (err) {
                 j(err)
