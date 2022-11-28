1400bd281495aff339a069fe0f2c5b66083c1355
Dezmou <incredibledez@gmail.com>
2022-11-28 09:45:13 +0100

    wip

--- lineup/src/Login/Login.tsx
+++ lineup/src/Login/Login.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { useRender } from '../render';
 import "./Login.css"
+import { Button, TextField } from "@mui/material"
+
 
 export default () => {
     const rd = useRender('login')
@@ -10,7 +12,30 @@ export default () => {
             backgroundImage: `url("/art/background.png")`,
         }}>
             <div className='login-field-cont'>
-
+                <div>
+                    <div className='login-field-div'>
+                        <TextField placeholder='User name' className='login-input' onChange={(e) => {
+                            console.log(e.target.value);
+                        }}></TextField>
+                    </div>
+                    <div className='login-field-div'>
+                        <TextField type="password" placeholder='Password' className='login-input' onChange={(e) => {
+                            console.log(e.target.value);
+                        }}></TextField>
+                    </div>
+                </div>
+                <div className='login-button-cont'>
+                    <div>
+                        <Button style={{
+                            backgroundColor: "#8a1414",
+                        }} className='login-button' variant='contained'>Login</Button>
+                    </div>
+                    <div>
+                        <Button style={{
+                            backgroundColor: "#8a1414",
+                        }} className='login-button' variant='contained'>New Account</Button>
+                    </div>
+                </div>
             </div>
         </div>
     </>
