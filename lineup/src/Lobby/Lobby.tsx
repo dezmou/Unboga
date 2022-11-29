51e9370f1d3274664ce9cc31c911af19cea97da6
Dezmou <incredibledez@gmail.com>
2022-11-29 12:34:37 +0100

    wip

--- lineup/src/Lobby/Lobby.tsx
+++ lineup/src/Lobby/Lobby.tsx
@@ -3,13 +3,65 @@ import { useRender } from '../render';
 import "./Lobby.css"
 import { Button, TextField } from "@mui/material"
 import { global } from '../state';
-import { State } from '../../back/src/common/api.interface';
+import { LobbyEntry, State } from '../../back/src/common/api.interface';
 import anime from "animejs"
 import { createUser, toast, login, challenge } from '../logic';
 
+const Challenge = (p: { challenge: LobbyEntry["challenge"] }) => {
+
+    return <>
+        <div className='challenge-main-cont' style={{
+            opacity: p.challenge ? "1" : "0",
+            pointerEvents: p.challenge ? "initial" : "none",
+        }}>
+            <div className='challenge-cont'>
+                <div>
+                    {p.challenge!.initiator === global.localState.user!.id && <>
+                        <div className='challenge-text'>
+                            You challenged {global.lobby[p.challenge!.player2].name} ({global.lobby[p.challenge!.player2].elo} ELO) !
+                        </div>
+                        <div className='challenge-buttons'>
+                            <div>
+                                <Button style={{
+                                    backgroundColor: "#8a1414",
+                                }} className='login-button' variant='contained'
+                                    onClick={() => { }}
+                                >Cancel</Button>
+                            </div>
+                        </div>
+                    </>}
+                    {p.challenge!.initiator !== global.localState.user!.id && <>
+                        <div className='challenge-text'>
+                            {global.lobby[p.challenge!.player1].name} ({global.lobby[p.challenge!.player1].elo} ELO) challenged you !
+                        </div>
+                        <div className='challenge-buttons-victim'>
+                            
+                            <div>
+                                <Button style={{
+                                    backgroundColor: "green",
+                                }} className='login-button' variant='contained'
+                                    onClick={() => { }}
+                                >Accept</Button>
+                            </div>
+                            <div style={{marginLeft : "10px"}}>
+                                <Button style={{
+                                    backgroundColor: "#8a1414",
+                                }} className='login-button' variant='contained'
+                                    onClick={() => { }}
+                                >Decline</Button>
+                            </div>
+                        </div>
+
+
+                    </>}
+                </div>
+            </div>
+        </div>
+    </>
+}
 
 export default () => {
-    const rd = useRender('lobby')
+    useRender('lobby')
     const lastPage = useRef(global.state.page);
 
     useEffect(() => {
@@ -23,17 +75,22 @@ export default () => {
     }
 
     return <>
-        <div className='lobby-main-cont' style={{
-            opacity: global.state.page === "lobby" ? "1" : 0,
-            pointerEvents: global.state.page === "lobby" ? "initial" : "none",
-        }}>
-            {Object.values(global.lobby).map((user, i) => <div key={i}>
-                <strong>{user.name}</strong>  (elo : {user.elo}) <span style={{ color: "green" }}>{user.status}</span>
-                <button onClick={() => {
-                    clickChallenge(user.id);
-                }}>challenge !</button>
-                {user.challenge && "CHALLENGE !"}
-            </div>)}
-        </div>
+        {global.state.page === "lobby" && global.lobby[global.localState.user!.id] && <>
+            <div className='lobby-main-cont' style={{
+                opacity: global.state.page === "lobby" ? "1" : 0,
+                pointerEvents: global.state.page === "lobby" ? "initial" : "none",
+            }}>
+                {Object.values(global.lobby).map((user, i) => <div key={i}>
+                    <strong>{user.name}</strong>  (elo : {user.elo}) <span style={{ color: "green" }}>{user.status}</span>
+                    <button onClick={() => {
+                        clickChallenge(user.id);
+                    }}>challenge !</button>
+                    {user.challenge && "CHALLENGE !"}
+                </div>)}
+            </div>
+            {global.lobby[global.localState.user!.id].challenge && <>
+                <Challenge challenge={global.lobby[global.localState.user!.id].challenge}></Challenge>
+            </>}
+        </>}
     </>
 }
